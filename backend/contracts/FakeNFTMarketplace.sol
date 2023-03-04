c0210439343cbcf01358fb83118e2de974b46385
Lario <osunkunleolanrewaju@gmail.com>
2023-03-04 10:21:49 +0100

    write deployment code

--- backend/contracts/FakeNFTMarketplace.sol
+++ backend/contracts/FakeNFTMarketplace.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 contract FakeNFTMarketplace {
     mapping(uint256 => address) public tokens;
 
-    uint256 nftPrice = 0.1 ether;
+    uint256 nftPrice = 0.001 ether;
 
     function purchase(uint256 _tokenId) external payable {
         require(msg.value == nftPrice, "This NFT costs 0.1 ether");
